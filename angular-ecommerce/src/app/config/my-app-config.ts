b1a4f70db53440920d96d3bfe337b8fee38e6f5f
Minadeo <minadeo.robin@gmail.com>
2021-05-25 00:48:43 +0200

    ajout du projet angular

---
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa37pujfWNtwvNgo5d6',
+        issuer: 'https://dev-7366730.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
